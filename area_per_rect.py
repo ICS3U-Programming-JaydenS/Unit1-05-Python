37fc7a96fe68e4bc8588b8cd86f52eaef23460d4
Jayden Smith <jayden.smith2@stu.ocsb.ca>
2025-02-19 03:44:12 +0000

    Added area_per_rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+#Created By: Jayden Smith
+#Date: Feb 18, 2025
+# This programs calculates the area and perimeter of a rectangle with the dimensions 11cmx7cm
+def main():
+    # this function displays the area and perimeter.
+    print("If the rectangle has the dimensions")
+    print("11cm x 7cm ")
+    print ("")
+    print ("Area is {}cm^2". format(11*7))
+    print ("Perimeter is {}cm^2". format((2*11)+(2*7)))
+
+
+if __name__== "__main__":
+    main()
+
